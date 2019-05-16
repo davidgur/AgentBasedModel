fa87dc7e17530ee6844c25d4a572805a54bd5dc2
David Gurevich <david@gurevich.ca>
2019-05-16 13:46:48 -0400

    Added program that gets the average R0 for simulations :chart_with_upwards_trend:

---
+++ tools/secondary_infections.py
@@ -0,0 +1,35 @@
+# Determining the R0 for a simulation
+
+# David Gurevich
+# david (at) gurevich (dot) ca
+
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+
+from glob import glob
+
+# Get all the secondary infection files
+r0_files = {}
+folders = glob(sys.argv[1])
+
+for folder in folders:
+    r0_files[folder[:-1]] = []
+
+    for i, (par_dir, _, _) in enumerate(os.walk(os.getcwd() + '/' + folder)):
+        if i != 0:
+            r0_files[folder[:-1]].append(par_dir + '/secondary_infections.csv')
+
+# Get the R0 data
+R0_data = []
+
+for _, r0_file_list in r0_files.items():
+    for r0_file in r0_file_list:
+        data = pd.read_csv(r0_file)
+        R0_data.append(data['SECONDARY_INFECTIONS'].head())
+
+# Print average
+average_r0 = np.mean([np.mean(x) for x in R0_data])
+print(average_r0)
