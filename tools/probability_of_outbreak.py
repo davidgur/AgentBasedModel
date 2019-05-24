47f11dd214e64a3ecf189c0ea3aba01013dfd679
David Gurevich <david@gurevich.ca>
2019-05-24 11:17:50 -0400

    Fixed fatal exception in analysis script :bug:

--- tools/probability_of_outbreak.py
+++ tools/probability_of_outbreak.py
@@ -16,24 +16,27 @@ for vacc_folder in vacc_folders:
             population_files[vacc_folder[:-1]].append(par_dir+"/population_sizes.csv")
 
 for sim_id, population_file_list in population_files.items():
-    outbreak_simulations_1 = 0
-    outbreak_simulations_2 = 0
-    outbreak_simulations_5 = 0
-    outbreak_simulations_10 = 0
-    for population_file in population_file_list:
-        data = pd.read_csv(population_file)
-        recovered_num = max([data['G9R'][data.index[-1]], data['G10R'][data.index[-1]], data['G11R'][data.index[-1]], data['G12R'][data.index[-1]] ])
-        if recovered_num > 1:
-            outbreak_simulations_1 += 1
-        if recovered_num > 2:
-            outbreak_simulations_2 += 1
-        if recovered_num > 5:
-            outbreak_simulations_5 += 1
-        if recovered_num > 10:
-            outbreak_simulations_10 += 1
+    if len(population_file_list) != 0:
+        outbreak_simulations_1 = 0
+        outbreak_simulations_2 = 0
+        outbreak_simulations_5 = 0
+        outbreak_simulations_10 = 0
+        for population_file in population_file_list:
+            data = pd.read_csv(population_file)
+            recovered_num = max([data['G9R'][data.index[-1]], data['G10R'][data.index[-1]], data['G11R'][data.index[-1]], data['G12R'][data.index[-1]] ])
+            if recovered_num > 1:
+                outbreak_simulations_1 += 1
+            if recovered_num > 2:
+                outbreak_simulations_2 += 1
+            if recovered_num > 5:
+                outbreak_simulations_5 += 1
+            if recovered_num > 10:
+                outbreak_simulations_10 += 1
 
-    outbreak_simulations_1 /= len(population_file_list)
-    outbreak_simulations_2 /= len(population_file_list)
-    outbreak_simulations_5 /= len(population_file_list)
-    outbreak_simulations_10 /= len(population_file_list)
-    print(sim_id[4:], outbreak_simulations_1, outbreak_simulations_2, outbreak_simulations_5, outbreak_simulations_10, sep=",")
+        outbreak_simulations_1 /= len(population_file_list)
+        outbreak_simulations_2 /= len(population_file_list)
+        outbreak_simulations_5 /= len(population_file_list)
+        outbreak_simulations_10 /= len(population_file_list)
+        print(sim_id[4:], outbreak_simulations_1, outbreak_simulations_2, outbreak_simulations_5, outbreak_simulations_10, sep=",")
+    else:
+        print(sim_id, " is not available.")
